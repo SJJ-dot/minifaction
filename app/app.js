026b1c0177762ebd08d36e75fbe7c7119a0844b9
SJJ <978622005@qq.com>
2018-06-02 13:18:31 +0800

    搜索

--- app/app.js
+++ app/app.js
@@ -5,33 +5,28 @@ App({
     // 登录
     wx.login({
       success: res => {
-       wx.request({
-         url: 'https://hishen.top/fs/auth',
-         data: {
-           code: res.code,
-         },
-         success: function (res) {
-           console.log(res)
-           if(res.statusCode == 200){
-             var authRes = res.data.status == 1 && res.data.data;
-             wx.setStorage({
-               key: "auth",
-               data: authRes
-             })
-             if (authRes)
-              getApp().globalData.baseUrl = "https://hishen.top/fs/";
-           }else {
-             if (wx.getStorageSync("auth"))
-              getApp().globalData.baseUrl = "https://hishen.top/fs/";
-           }
-         },
-       })
+        wx.request({
+          url: 'https://hishen.top/fs/auth',
+          data: {
+            code: res.code,
+          },
+          success: function (res) {
+            if (res.statusCode == 200) {
+              var authRes = res.data.status == 1 && res.data.data;
+              wx.setStorage({
+                key: "auth",
+                data: authRes
+              })
+              if (authRes)
+                getApp().data.baseUrl = "https://hishen.top/fs/";
+            } else {
+              if (wx.getStorageSync("auth"))
+                getApp().data.baseUrl = "https://hishen.top/fs/";
+            }
+          }
+        })
       }
     })
   },
-  globalData: {
-    userInfo: {
-     
-    }
-  }
+  data: {}
 })
--- app/pages/search/search.js
+++ app/pages/search/search.js
@@ -5,90 +5,55 @@ Page({
    * 页面的初始数据
    */
   data: {
-    bookList:[],
+    inputShowed: false,
+    inputVal: "",
+    bookList: [{ "name": "极道天魔", "author": "滚开", "books": [{ "url": "https://www.aszw.org/book/168/168625/", "name": "极道天魔", "author": "滚开", "bookCoverImgUrl": "", "intro": "", "chapterList": [] }, { "url": "https://www.biquge5200.cc/58_58206/", "name": "极道天魔", "author": "滚开", "bookCoverImgUrl": "", "intro": "", "chapterList": [] }, { "url": "https://www.dhzw.org/book/165/165908/", "name": "极道天魔", "author": "滚开", "bookCoverImgUrl": "", "intro": "", "chapterList": [] }, { "url": "http://www.yunlaige.com/book/19984.html", "name": "极道天魔", "author": "滚开", "bookCoverImgUrl": "", "intro": "", "chapterList": [] }] }],
   },
   searchBook:function(arg){
     var searchPage = this;
-    wx.showLoading({
-      title: '加载中……',
-    })
+    wx.showToast({
+      title: '数据加载中',
+      icon: 'loading',
+      duration: 3000
+    });
     wx.request({
-      url: getApp().globalData.baseUrl+'/search', //仅为示例，并非真实的接口地址
+      url: getApp().data.baseUrl+'/search', //仅为示例，并非真实的接口地址
       data: {
         searchKey: arg.detail.value.trim(),
       },
       success: function (res) {
-        wx.hideLoading()
         if(res.data.status == 1){
           console.log(res.data.data)
           searchPage.setData({ bookList: res.data.data })
         }else{
           wx.showToast({
             title: res.data.errorMsg,
-          })
+            icon: 'success',
+            duration: 1000
+          });
         }
-      },
-      fail:function(res){
-        wx.showToast({
-          title: "fail",
-        })
-        wx.hideLoading()
       }
     })
   },
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
+  showInput: function () {
+    this.setData({
+      inputShowed: true
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
+  hideInput: function () {
+    this.setData({
+      inputVal: "",
+      inputShowed: false
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
+  clearInput: function () {
+    this.setData({
+      inputVal: ""
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  inputTyping: function (e) {
+    this.setData({
+      inputVal: e.detail.value
+    });
   }
 })
