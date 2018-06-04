46cc1104f596949d0a587e74aba9ef7445607cac
SJJ <978622005@qq.com>
2018-06-04 22:46:42 +0800

    0.0.9

--- fiction/src/main/kotlin/com/sjj/fiction/model/Auth.kt
+++ fiction/src/main/kotlin/com/sjj/fiction/model/Auth.kt
@@ -9,7 +9,7 @@ import java.io.OutputStream
 import java.util.*
 
 @ConfigurationProperties(prefix = "auth")
-@PropertySource("classpath:auth.properties")
+@PropertySource("file:auth.properties")
 @Component
 class Auth {
     var ignoreVersions = mutableSetOf<String>()
